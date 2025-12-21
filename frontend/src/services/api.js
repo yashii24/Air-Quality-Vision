717bb49706ad7d7120999ff840b12244af86fa82
yashii24 <yashikardam2004@gmail.com>
2025-12-22 00:32:36 +0530

    updte api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+alert("API FILE LOADED — BASE URL = " + import.meta.env.VITE_API_BASE_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
