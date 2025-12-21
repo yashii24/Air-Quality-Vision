1cc748caa19dd8a7c0a41478248ff4fcba548488
yashii24 <yashikardam2004@gmail.com>
2025-12-21 23:59:14 +0530

    :update service/api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-console.log(
-  "API BASE (prod):",
-  import.meta.env.VITE_API_BASE_URL
-);
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
