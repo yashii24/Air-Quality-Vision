1d0fcbaadfb43e18213cd4037fbc35cc21500c87
yashii24 <yashikardam2004@gmail.com>
2025-12-21 23:47:04 +0530

    verify VITE_API_BASE_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+console.log(
+  "API BASE (prod):",
+  import.meta.env.VITE_API_BASE_URL
+);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
 });
